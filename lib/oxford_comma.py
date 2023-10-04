a9d7eb89a2ecbddba159a1e973b2acb563101034
preciousnjukia <preciousnjukia@gmail.com>
2023-10-04 18:40:48 +0300

    Assignment done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,13 @@
 def oxford_comma(items):
-    return None
+    x = "".join(items)
+    return x
+
+def oxford_comma(items):
+    if len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    elif len(items) > 2:
+        last_item = items[-1]
+        items_except_last = items[:-1]
+        return f"{', '.join(items_except_last)}, and {last_item}"
+    else:
+        return ", ".join(items)
